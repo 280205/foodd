b3531a814f490a9c2260785b4297c1b24df618d2
NITIN PANDEY <109999762+280205@users.noreply.github.com>
2025-07-16 19:37:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,34 @@
-import express from "express";
-import cors from "cors";
-import { connectDB } from "./config/db.js";
-import foodRouter from "./routes/foodRoute.js";
-import userRouter from "./routes/userRoute.js";
-import "dotenv/config";
-import cartRouter from "./routes/cartRoute.js";
-import orderRouter from "./routes/orderRoute.js";
+// ✅ Must be first
+import 'dotenv/config'
 
-// app config
-const app = express();
-const port =process.env.PORT || 4000;
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
-//middlewares
-app.use(express.json());
-app.use(cors());
+const app = express()
+const port = 4000
 
-// DB connection
-connectDB();
+app.use(express.json())
+app.use(cors())
 
-// api endpoints
-app.use("/api/food", foodRouter);
-app.use("/images", express.static("uploads"));
-app.use("/api/user", userRouter);
-app.use("/api/cart", cartRouter);
-app.use("/api/order", orderRouter);
+// DB Connection
+connectDB()
+
+// API Endpoints
+app.use("/api/food", foodRouter)
+app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
 
 app.get("/", (req, res) => {
-  res.send("API Working");
-});
+  res.send("API Working")
+})
 
 app.listen(port, () => {
-  console.log(`Server Started on port: ${port}`);
-});
+  console.log(`Server Started on http://localhost:${port}`)
+})
