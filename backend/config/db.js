42f55261cb2431acf0e8ace677adad6734c657e3
NITIN PANDEY <109999762+280205@users.noreply.github.com>
2025-07-16 19:44:23 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,13 @@
+// config/db.js
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
+  console.log("💥 MONGO_URI =", process.env.MONGO_URI); // Add this line to confirm
   await mongoose
-    .connect(
-      process.env.MONGO_URL
-    )
-    .then(() =>console.log("DB Connected"));
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("✅ DB Connected"))
+    .catch((err) => {
+      console.error("❌ DB Connection Error:", err.message);
+      process.exit(1);
+    });
 };
